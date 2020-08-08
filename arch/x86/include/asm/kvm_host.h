49848e8ae7dbb38f5489474d7042843660492cd8
Jason Zeng <jason.zeng@intel.com>
2020-08-08 22:11:20 +0800

    x86: support irq_bypass_save/restore_consumer callback
    Introduce x86 interface to support irq_bypass save/restore consumer
    callback interface.
    
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1384,6 +1384,8 @@ struct kvm_x86_ops {
 	int (*complete_emulated_msr)(struct kvm_vcpu *vcpu, int err);
 
 	void (*vcpu_deliver_sipi_vector)(struct kvm_vcpu *vcpu, u8 vector);
+
+	int (*pi_do_keepalive)(struct kvm *kvm, int guest_irq, void **data, bool save);
 };
 
 struct kvm_x86_nested_ops {
