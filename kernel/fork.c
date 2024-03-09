d6363d4cebbf6665e8cf67267cc0891efa458ebe
Pasha Tatashin <pasha.tatashin@soleen.com>
2024-03-09 16:32:06 +0000

    fork: Remove assumption that vm_area->nr_pages equals to THREAD_SIZE
    In many places number of pages in the stack is detremined via
    (THREAD_SIZE / PAGE_SIZE). There is also a BUG_ON() that ensures that
    (THREAD_SIZE / PAGE_SIZE) is indeed equals to vm_area->nr_pages.
    
    However, with dynamic stacks, the number of pages in vm_area will grow
    with stack, therefore, use vm_area->nr_pages to determine the actual
    number of pages allocated in stack.
    
    Signed-off-by: Pasha Tatashin <pasha.tatashin@soleen.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -245,13 +245,11 @@ static int free_vm_stack_cache(unsigned int cpu)
 
 static int memcg_charge_kernel_stack(struct vm_struct *vm_area)
 {
-	int i;
-	int ret;
+	int i, ret, nr_pages;
 	int nr_charged = 0;
 
-	BUG_ON(vm_area->nr_pages != THREAD_SIZE / PAGE_SIZE);
-
-	for (i = 0; i < THREAD_SIZE / PAGE_SIZE; i++) {
+	nr_pages = vm_area->nr_pages;
+	for (i = 0; i < nr_pages; i++) {
 		ret = memcg_kmem_charge_page(vm_area->pages[i], GFP_KERNEL, 0);
 		if (ret)
 			goto err;
@@ -533,9 +531,10 @@ static void account_kernel_stack(struct task_struct *tsk, int account)
 {
 	if (IS_ENABLED(CONFIG_VMAP_STACK)) {
 		struct vm_struct *vm_area = task_stack_vm_area(tsk);
-		int i;
+		int i, nr_pages;
 
-		for (i = 0; i < THREAD_SIZE / PAGE_SIZE; i++) {
+		nr_pages = vm_area->nr_pages;
+		for (i = 0; i < nr_pages; i++) {
 			mod_lruvec_page_state(vm_area->pages[i],
 					      NR_KERNEL_STACK_KB,
 					      account * (PAGE_SIZE / 1024));
@@ -554,11 +553,11 @@ void exit_task_stack_account(struct task_struct *tsk)
 	account_kernel_stack(tsk, -1);
 
 	if (IS_ENABLED(CONFIG_VMAP_STACK)) {
-		struct vm_struct *vm_area;
-		int i;
+		struct vm_struct *vm_area = task_stack_vm_area(tsk);
+		int i, nr_pages;
 
-		vm_area = task_stack_vm_area(tsk);
-		for (i = 0; i < THREAD_SIZE / PAGE_SIZE; i++)
+		nr_pages = vm_area->nr_pages;
+		for (i = 0; i < nr_pages; i++)
 			memcg_kmem_uncharge_page(vm_area->pages[i], 0);
 	}
 }
